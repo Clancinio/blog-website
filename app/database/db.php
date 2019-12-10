74617660ed87d36949f0301a669d0fea572fa8aa
Dean <dean.clancy91@gmail.com>
2019-12-10 12:51:20 -0800

    Added conditions param to selectAll method in db.php

--- app/database/db.php
+++ app/database/db.php
@@ -10,15 +10,44 @@ function displayData($values) // to be deleted
     die();
 }
 
-function selectAll($table) // selects all records from a specified table
+function selectAll($table, $conditions = []) // selects all records from a specified table
 {
     global  $conn;
     $sql = "SELECT * FROM $table";
-    $statement = $conn->prepare($sql);
-    $statement->execute();
-    $records = $statement->get_result()->fetch_all(MYSQLI_ASSOC);
-    return $records;
+    if (empty($conditions)) {
+        $statement = $conn->prepare($sql);
+        $statement->execute();
+        $records = $statement->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $records;
+    } else {
+        // return records that match conditions
+        //$sql = "SELECT * FROM $table WHERE username = 'Dean' AND admin = 1";
+
+        $i = 0;
+        foreach ($conditions as $key => $value) {
+            if ($i == 0) {
+                $sql = $sql . " WHERE $key = ?";
+            } else {
+                $sql = $sql . " AND $key = ?";
+            }
+            $i++;
+        }
+
+        $statement = $conn->prepare($sql);
+        $values = array_values($conditions);
+        $types = str_repeat('s', count($value));
+        $statement->bind_param($types, ...$values);
+        $statement->execute();
+        $records = $statement->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $records;
+    }
 }
 
-$users = selectAll('users');
+$conditions = [
+    //key     value
+    'admin' => 1,
+    'username' => 'beano'
+];
+
+$users = selectAll('users', $conditions);
 displayData($users);
